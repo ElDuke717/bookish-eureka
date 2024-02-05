897fddefdaeb73d86eb0114cf12e378c8435b476
elduke717 <spacematcha@proton.me>
2024-02-05 14:22:06 -0500

    add Beatles

---
+++ Beatles.java
@@ -0,0 +1,19 @@
+public class Beatles {
+    
+    public static void line1() {
+        System.out.println("Hello, Hello");
+    }
+    public static void line2() {
+        System.out.println("I don't know why you say goodbye,");
+    }
+    public static void line3() {
+        System.out.println("I say hello.");
+    }
+       
+
+    public static void main(String[] args) {
+        line1();
+        line2();
+        line3();        
+    }
+}
